5421def6c68e8c85496381d6b1430105d2f55db1
redgtxt <hp052635@gmail.com>
2026-05-13 12:04:26 +0100

    cpp07 done

--- cpp07/ex01/src/main.cpp
+++ cpp07/ex01/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "iter.hpp"
 
-// Funções de teste
 void printInt(int &n) {
     std::cout << n << " ";
 }
@@ -28,33 +27,33 @@ void printSquare(int &n) {
 }
 
 int main() {
-    // Teste 1: array de ints - imprimir
+    // Print array
     std::cout << "=== Teste 1: Print ints ===" << std::endl;
     int arr[] = {1, 2, 3, 4, 5};
     size_t len = sizeof(arr) / sizeof(arr[0]);
     iter(arr, len, printInt);
     std::cout << std::endl;
 
-    // Teste 2: array de ints - dobrar valores
+    // Appy funtion doubleInt
     std::cout << "=== Teste 2: Dobrar ints ===" << std::endl;
     iter(arr, len, doubleInt);
     iter(arr, len, printInt);
     std::cout << std::endl;
 
-    // Teste 3: array de strings
+    //Array de strings
     std::cout << "=== Teste 3: Print strings ===" << std::endl;
     std::string words[] = {"hello", "world", "42"};
     iter(words, 3, printStr);
     std::cout << std::endl;
 
-    // Teste 4: array de chars com toUpper
+    //ToUpper with array of chars
     std::cout << "=== Teste 4: toUpper chars ===" << std::endl;
     char letters[] = {'a', 'b', 'c', 'd'};
     iter(letters, 4, toUpper);
     iter(letters, 4, printChar);
     std::cout << std::endl;
 
-    // Teste 5: quadrado dos valores
+    // Square of number
     std::cout << "=== Teste 5: Quadrado ===" << std::endl;
     int nums[] = {3, 4, 5};
     iter(nums, 3, printSquare);
